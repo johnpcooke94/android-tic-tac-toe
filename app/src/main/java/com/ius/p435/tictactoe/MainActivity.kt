7b0e181ef5ede05a388312323ae1a5d879456fc5
John Cooke <john.pcooke94@gmail.com>
2021-08-28 13:49:32 -0400

    Set up basic turn cycle for top left button

--- app/src/main/java/com/ius/p435/tictactoe/MainActivity.kt
+++ app/src/main/java/com/ius/p435/tictactoe/MainActivity.kt
@@ -2,10 +2,57 @@ package com.ius.p435.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class TurnTracker {
+
+    private val turns: Array<String> = arrayOf("x", "o")
+    private var currentTurn: Int = 0
+
+    fun getCurrentTurn(): String {
+        return turns[currentTurn]
+    }
+
+    fun iterateTurn() {
+        if (currentTurn == 1) {
+            currentTurn = 0
+        }
+        else {
+            currentTurn++
+        }
+    }
+
+}
 
 class MainActivity : AppCompatActivity() {
+
+    private val turnTracker = TurnTracker()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topLeftButton = findViewById<Button>(R.id.top_left)
+        topLeftButton.setOnClickListener {onButtonClick(topLeftButton)}
+    }
+
+    private fun onButtonClick(button: Button) {
+        if (button.text == "") {
+
+            if (turnTracker.getCurrentTurn().lowercase() == "x") {
+                button.text = "X"
+            }
+            else {
+                button.text = "Y"
+            }
+
+            turnTracker.iterateTurn()
+
+        }
+        else {
+            Toast.makeText(this@MainActivity, "That square has already been used!",
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
